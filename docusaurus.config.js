b3bb01cb3d260ad0c3e2e197ab48b986ec68e125
Philip Ellis <philip.ellis.j@gmail.com>
2023-06-12 18:00:52 -0400

    named properly

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/philipellisis/csd-docs/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/philipellisis/csd-docs/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/philipellisis/csd-docs/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/philipellisis/csd-docs/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),

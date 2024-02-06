3a51da6ba9953b0219e680cc38d4756acc6dab8a
philip ellis <philip.ellis.j@gmail.com>
2024-02-06 15:40:59 -0500

    updated production url to the right path

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://clevelandsoftwaredesign.com',
+  url: 'https://pinball-docs.clevelandsoftwaredesign.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
